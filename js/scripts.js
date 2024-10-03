1630b8bd4cea8399edc8acc585cbb173d5191e1f
DC Isenberg <dclem742@gmail.com>
2024-10-03 09:15:15 -0400

    first branch commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,42 @@
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('DOM Ready...FAMILY');
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("DOM Ready...FAMILY");
 
-    const rootDiv = document.querySelector('#root');
-    const button = document.createElement('button');
-    button.innerText = 'CLICK ME!';
-    button.classList.add('button')
+  const rootDiv = document.querySelector("#root");
+  const button = document.createElement("button");
+  button.innerText = "CLICK ME!";
+  button.classList.add("button");
 
-    button.addEventListener('click', function (e) {
-        e.preventDefault();
-        fetchQuote();
-    });
-
-    rootDiv.appendChild(button);
-
-    const url = 'https://api.chucknorris.io/jokes/random';
-    const options = { method: 'GET' };
-
-    function fetchQuote() {
-        fetch(url, options)
-            .then(function (response) {
-                console.log('RESPONSE: ', response);
-                return response.json();
-            })
-            .then(function (data) {
-                return generateQuote(data);
-            });
-    }
-
-    function generateQuote(data) {
-        console.log('DATA: ', data);
-
-        const paragraph = document.createElement('p');
-        paragraph.innerHTML = data.value;
-        console.log('PARAGRAPH? ', paragraph);
-        rootDiv.appendChild(paragraph);
-    }
-
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
     fetchQuote();
+  });
+
+  rootDiv.appendChild(button);
+
+  const url = "https://api.chucknorris.io/jokes/random";
+  const options = { method: "GET" };
+
+  async function fetchQuote() {
+    // fetch(url, options)
+    //     .then(function (response) {
+    //         console.log('RESPONSE: ', response);
+    //         return response.json();
+    //     })
+    //     .then(function (data) {
+    //         return generateQuote(data);
+    //     });
+    const data = await fetch(url, options).then((response) => response.json());
+    generateQuote(data);
+  }
+
+  function generateQuote(data) {
+    console.log("DATA: ", data);
+
+    const paragraph = document.createElement("p");
+    paragraph.innerHTML = data.value;
+    console.log("PARAGRAPH? ", paragraph);
+    rootDiv.appendChild(paragraph);
+  }
+
+  fetchQuote();
 });
